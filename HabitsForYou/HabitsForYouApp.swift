8b4367d7b3b7cd6a65fdae15eebf2c19f105d87f
Eugene Cross <eugene.cross@gmail.com>
2023-08-29 10:34:12 +0100

    Initial Commit

---
+++ HabitsForYou/HabitsForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsForYouApp.swift
+//  HabitsForYou
+//
+//  Created by Eugene on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitsForYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
